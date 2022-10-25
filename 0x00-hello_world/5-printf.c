782b252cb7757252b63104a61253ad2a1536a68b
Tekashi-cole69 <ibehangel13@gmail.com>
2022-10-25 15:25:30 -0700

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints the string
+ *
+ *   *
+ *
+ *    * Description: prints Programming is like building a multilingual puzzle.
+ *
+ *     * Return: Always 0 (Success)
+ *
+ *      */
+
+
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints the string
+ *
+ *   *
+ *
+ *    * Description: prints Programming is like building a multilingual puzzle.
+ *
+ *     * Return: Always 0 (Success)
+ *
+ *      */
+
+
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
+
+
